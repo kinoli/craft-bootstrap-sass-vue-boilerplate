51750cee686fd427ab4efe2ab8ef8239dbf3bcdb
Jesse Knowles <jessek@opisystems.com>
2020-05-18 15:00:38 -0600

    Improve eslint/prettier usage

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,28 +2,43 @@ module.exports = {
   root: true,
   // parser: 'babel-eslint',
   parserOptions: {
-    sourceType: "module",
+    sourceType: 'module'
   },
   env: {
+    es6: true,
     browser: true,
-    node: true,
+    node: true
   },
   extends: [
     // add more generic rulesets here, such as:
-    // 'eslint:recommended',
-    "plugin:vue/recommended",
+    'plugin:prettier/recommended', // turns off all ESLINT rules that are unnecessary due to Prettier or might conflict with Prettier.
+    'plugin:vue/recommended', // /base, /essential, /strongly-recommended, /recommended
+    'prettier/vue',
+    'prettier/standard'
   ],
   globals: {
-    __static: true,
+    __static: true
   },
-  plugins: ["html"],
+  plugins: ['html', 'simple-import-sort', 'prettier', 'vue'],
   rules: {
+    'simple-import-sort/sort': 'error',
     // allow paren-less arrow functions
-    "arrow-parens": 0,
+    'arrow-parens': 0,
+    // "max-depth": [1, 3],
     // allow async-await
-    "generator-star-spacing": 0,
+    'generator-star-spacing': 0,
     // allow debugger during development
-    "no-debugger": process.env.NODE_ENV === "production" ? 2 : 0,
-    "object-curly-spacing": [2, "always"],
-  },
-};
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'object-curly-spacing': [2, 'always'],
+    'max-len': [
+      'warn',
+      {
+        code: 120,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreComments: true
+      }
+    ],
+    'vue/max-attributes-per-line': 'off'
+  }
+}
